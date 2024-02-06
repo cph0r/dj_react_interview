ba6f42f472638a7dfeafb6b8e3a47788149cb8a1
chiragphor <chiragphor19@gmail.com>
2024-02-06 18:04:47 +0530

    added files

---
+++ frontend/djreact/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FindTrucks from "./findTrucks";
+
+function App() {
+  return (
+    <div className="App">
+      <FindTrucks />
+    </div>
+  );
+}
+
+export default App;
